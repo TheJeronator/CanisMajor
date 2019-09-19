000ee5cf7ab7dae9c5a7137de1c304833062a738
TheJeronator <ensing.hj@hotmail.com>
2019-09-19 14:04:12 +0200

    Update 1

---
+++ Nebula Strike/Assets/Scripts/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    var speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var xmove = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+    }
+}
